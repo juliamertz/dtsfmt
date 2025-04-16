c029563b4cea70455ccfb472822f3835930cc491
Julia Mertz <info@juliamertz.dev>
2025-04-16 21:09:32 +0200

    format

--- flake.nix
+++ flake.nix
@@ -14,79 +14,74 @@
       flake = false;
     };
   };
-  outputs =
-    { nixpkgs, flake-parts, ... }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = {
+    nixpkgs,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = nixpkgs.lib.systems.flakeExposed;
 
-      perSystem =
-        {
-          config,
-          pkgs,
-          lib,
-          system,
-          ...
-        }:
-        let
-          nativeBuildInputs =
-            with pkgs;
-            [
-              pkg-config
-              rustPlatform.bindgenHook
-            ]
-            ++ lib.optionals stdenv.isDarwin [ makeBinaryWrapper ];
-          buildInputs =
-            with pkgs;
-            [ openssl ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [ IOKit ]);
+      perSystem = {
+        config,
+        pkgs,
+        lib,
+        system,
+        ...
+      }: let
+        nativeBuildInputs = with pkgs;
+          [
+            pkg-config
+            rustPlatform.bindgenHook
+          ]
+          ++ lib.optionals stdenv.isDarwin [makeBinaryWrapper];
+        buildInputs = with pkgs;
+          [openssl] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [IOKit]);
+      in {
+        packages.default = let
+          manifest = (lib.importTOML ./Cargo.toml).package;
         in
-        {
-          packages.default =
-            let
-              manifest = (lib.importTOML ./Cargo.toml).package;
-            in
-            pkgs.rustPlatform.buildRustPackage {
-              inherit buildInputs nativeBuildInputs;
-              inherit (manifest) name version;
+          pkgs.rustPlatform.buildRustPackage {
+            inherit buildInputs nativeBuildInputs;
+            inherit (manifest) name version;
 
-              src = pkgs.runCommand "dtsfmt-src" { } ''
-                mkdir -p $out/tree-sitter-devicetree
-                cp -r ${./.}/* $out
-                cp -r ${inputs.tree-sitter-devicetree}/* $out/tree-sitter-devicetree
-              '';
+            src = pkgs.runCommand "dtsfmt-src" {} ''
+              mkdir -p $out/tree-sitter-devicetree
+              cp -r ${./.}/* $out
+              cp -r ${inputs.tree-sitter-devicetree}/* $out/tree-sitter-devicetree
+            '';
 
-              cargoLock = {
-                lockFile = ./Cargo.lock;
-                allowBuiltinFetchGit = true;
-              };
-
-              meta.mainProgram = manifest.name;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              allowBuiltinFetchGit = true;
             };
 
-          devShells.default = pkgs.mkShell {
-            name = "dev-shell";
-            inherit nativeBuildInputs;
-
-            buildInputs =
-              let
-                overlays = [ (import inputs.rust-overlay) ];
-                pkgs = import (inputs.nixpkgs) { inherit system overlays; };
-              in
-              buildInputs
-              ++ (with pkgs.rust-bin; [
-                (stable.latest.minimal.override {
-                  extensions = [
-                    "clippy"
-                    "rust-src"
-                  ];
-                })
-
-                nightly.latest.clippy
-                nightly.latest.rustfmt
-                nightly.latest.rust-analyzer
-              ]);
+            meta.mainProgram = manifest.name;
           };
 
+        devShells.default = pkgs.mkShell {
+          name = "dev-shell";
+          inherit nativeBuildInputs;
+
+          buildInputs = let
+            overlays = [(import inputs.rust-overlay)];
+            pkgs = import (inputs.nixpkgs) {inherit system overlays;};
+          in
+            buildInputs
+            ++ (with pkgs.rust-bin; [
+              (stable.latest.minimal.override {
+                extensions = [
+                  "clippy"
+                  "rust-src"
+                ];
+              })
+
+              nightly.latest.clippy
+              nightly.latest.rustfmt
+              nightly.latest.rust-analyzer
+            ]);
         };
+      };
     };
 
   nixConfig = {
